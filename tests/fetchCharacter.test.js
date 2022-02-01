e836b1d26f8aaa1d2e6093bc0c24f6ec5e6e3f16
lazaro <lazaroandriola@gmail.com>
2022-02-01 14:49:56 -0300

    Adicionado testes conforme exercicios

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,22 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  it('Verifica se o nome da personagem é Wonder Woman', async () => {
+    const character = await fetchCharacter('720');
+    expect(character.name).toBe('Wonder Woman')
+  });
+  it('Retorno de erro caso não tenha passado o ID', async () => {
+    const erro = await fetchCharacter();
+    expect(erro).toEqual(new Error('You must provide an url'));
+  });
+  it('Verifica se retorna \'Invalid id\' ao executar a função com parâmetro que não existe', async () => {
+    const response = await fetchCharacter('parametro qualquer');
+    expect(response).toBe('Invalid id');
+  });
+  it('Verifica se fetch é chamada com o endpoint correto', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
 });
